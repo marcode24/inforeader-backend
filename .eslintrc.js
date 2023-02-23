2425de8f833c4bbbd53276b66216029354124bd2
marco cruz <marco24cruz08@gmail.com>
2023-02-22 19:49:24 -0600

    :seedling: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'max-len': ['error', { code: 90 }],
+    'linebreak-style': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off',
+    'no-unused-expressions': 'off',
+    'no-return-assign': 'off',
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'no-tabs': 'error',
+    semi: ['error', 'always'],
+    'space-before-blocks': 'error',
+  },
+};
