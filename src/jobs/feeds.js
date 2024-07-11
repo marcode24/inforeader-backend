e8f9081ca910b0476200ef192d489d10e0e860ba
Marco Cruz <marco24cruz08@gmail.com>
2024-07-10 23:37:25 -0600

    🔥 Remove unused feeds.js file

--- src/jobs/feeds.js
+++
@@ -1,6 +0,0 @@
-const { updateFeedRssItems } = require('../helpers/getFeedRss');
-
-export default function handler(req, res) {
-  updateFeedRssItems();
-  res.status(200).json({ ok: true, msg: 'Job started successfully' });
-}
